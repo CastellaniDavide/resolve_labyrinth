5959eaf1ac12a7b7fb0ea1051668813cf3dba40c
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2020-09-09 09:40:45 +0200

    Created bin/template.c

---
+++ bin/resolve_labyrinth.c
@@ -0,0 +1,22 @@
+/**
+ * @file resolve_labyrinth.cpp
+ *
+ * @version 01.01 2020-08-15
+ *
+ * @brief 
+ *
+ * @ingroup resolve_labyrinth
+ * (Note: this needs exactly one @defgroup somewhere)
+ *
+ * @author Castellani Davide
+ *
+ * Contact: contacts@castellanidavide.it
+ *
+ */
+#include <stdio.h>
+
+int main() {
+	// printf() displays the string inside quotation
+	printf("resolve_labyrinth");
+	return 0;
+}
